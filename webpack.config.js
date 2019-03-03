c19800e2c7add4df398d47a26ee5224dd830fd23
Mike Burke <michael.burke@wzukltd.com>
2019-03-03 20:40:34 +0000

    Removed unused loaders from webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -42,28 +42,7 @@ module.exports = {
         test:    /\.ts/,
         exclude: /node_modules\/(?!phusion|vue).*/,
         loaders: ['ts-loader']
-      },
-      {
-        test:   /\.html$/,
-        loader: 'html-loader'
-      },
-      {
-        test:    /\.scss$/,
-        loaders: ["style", "css", "sass"]
-      },
-      {
-        test:    /\.yaml$/,
-        include: path.resolve('data'),
-        loader:  'yaml'
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader'
       }
-      // {
-      //   test: /\.json$/,
-      //   loader: 'raw-loader'
-      // }
     ]
   }
 };
